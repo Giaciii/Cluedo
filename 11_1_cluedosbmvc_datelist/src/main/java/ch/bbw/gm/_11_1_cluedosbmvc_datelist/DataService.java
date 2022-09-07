a6a18bf44fa855e1c1d0beefdfaaf4140f231c1f
Giacii <93996172+Giaciii@users.noreply.github.com>
2022-09-07 09:22:17 +0200

    Add Person.java and add privat Strings, int and setter getter toString methods

---
+++ 11_1_cluedosbmvc_datelist/src/main/java/ch/bbw/gm/_11_1_cluedosbmvc_datelist/DataService.java
@@ -0,0 +1,4 @@
+package ch.bbw.gm._11_1_cluedosbmvc_datelist;
+
+public class DataService {
+}
---
+++ 11_1_cluedosbmvc_datelist/src/main/java/ch/bbw/gm/_11_1_cluedosbmvc_datelist/Person.java
@@ -0,0 +1,83 @@
+package ch.bbw.gm._11_1_cluedosbmvc_datelist;
+
+/**
+ * @author Giacun
+ * @version 07.09.2022
+ */
+public class Person {
+    private String name;
+    private String anrede;
+    private String haarfarbe;
+    private int alter;
+    private int groesse;
+    private String merkmal;
+
+    public Person(String name, String anrede, String haarfarbe, int alter, int groesse, String merkmal) {
+        this.name = name;
+        this.anrede = anrede;
+        this.haarfarbe = haarfarbe;
+        this.alter = alter;
+        this.groesse = groesse;
+        this.merkmal = merkmal;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAnrede(String anrede) {
+        this.anrede = anrede;
+    }
+
+    public void setHaarfarbe(String haarfarbe) {
+        this.haarfarbe = haarfarbe;
+    }
+
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+
+    public void setGroesse(int groesse) {
+        this.groesse = groesse;
+    }
+
+    public void setMerkmal(String merkmal) {
+        this.merkmal = merkmal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAnrede() {
+        return anrede;
+    }
+
+    public String getHaarfarbe() {
+        return haarfarbe;
+    }
+
+    public int getAlter() {
+        return alter;
+    }
+
+    public int getGroesse() {
+        return groesse;
+    }
+
+    public String getMerkmal() {
+        return merkmal;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", anrede='" + anrede + '\'' +
+                ", haarfarbe='" + haarfarbe + '\'' +
+                ", alter=" + alter +
+                ", groesse=" + groesse +
+                ", merkmal='" + merkmal + '\'' +
+                '}';
+    }
+}
